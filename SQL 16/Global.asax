b2a07e65c85cec02d165227c00c538ae679e4771
mohdhassan7890@gmail.com <mohdhassan7890@gmail.com>
2023-09-13 14:11:25 +0530

    First commit

---
+++ SQL 16/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SQL_16.MvcApplication" Language="C#" %>
---
+++ SQL 16/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SQL_16.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Due Date is required.")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+    }
+}
